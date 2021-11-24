f71297b7a322378ca5201494a2b5e2b79f9ad870
ArtemKuznetsov44 <kuzia.artem456485@mail.ru>
2021-11-24 16:41:26 +0300

    Homework for lesson-3

---
+++ HomeTask_3/BankAccount.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask_3
+{
+    class BankAccount
+    {
+        private static int _counter = 0;
+        private int _accountNumber { get; set; } // id.
+        private double _balance { get; set; } // Баланс.
+        private Enum _acountType { get; set; } // Тип банковского счета.
+
+        #region //Конструкторы.
+        public BankAccount() // Без параметров - устанавливаем значение id по умолчанию.
+        {
+            this._accountNumber = GetDefNumber();
+        }
+        public BankAccount(int accountNumber) // При передаче id - устанавливаем переданное id.
+        {
+            this._accountNumber = accountNumber; 
+        }
+        public BankAccount(double balance) : this() // При передаче баланаса - устанавливаем значение id по умолчанию и переданный баланс.
+        {
+            this._balance = balance; 
+        }
+        public BankAccount (Enum type): this() // При передаче типа счета - уставливаем значение id по умолчанию и преданный тип счета.
+        {
+            this._acountType = type; 
+        }
+        public BankAccount(int accountNumber, double balance, Enum type) // Если переданы сразу три параметра.
+        {
+            this._accountNumber = accountNumber;
+            this._balance = balance;
+            this._acountType = type;
+        }
+        #endregion
+
+        public void MoneyTaransaction(BankAccount account, double sum) // Перевод средств с одного счета на другой.
+        {
+            if (sum <= this.Balance)
+            {
+                this._balance -= sum;
+                account._balance += sum;
+            }
+            else
+                Console.WriteLine("Недостаточно средств ! Транзакция отменена."); 
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"Номер банковского счета: {AccountNumber}");
+            Console.WriteLine($"Баланс: {Balance}");
+            Console.WriteLine($"Тип банковского счета: {AccountType}");
+        }
+        private int GetDefNumber() // Увеличиваем значение нашего счетчика (деффолтного id).
+        {
+            return ++_counter;
+        }
+
+        #region //Свойства полей.
+        public int AccountNumber
+        {
+            get { return _accountNumber; }
+            set { _accountNumber = value; }
+        }
+        public double Balance
+        {
+            get { return _balance; }
+            set { _balance = value; }
+        }
+        public Enum AccountType
+        {
+            get { return _acountType; }
+            set { _acountType = value; }
+        }
+        #endregion
+    }
+}
---
+++ HomeTask_3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace HomeTask_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account_1 = new(23, 37500.00, AccountType.BudgetAccount); // Создаем первый аккаунт.
+            BankAccount account_2 = new(2500.00); // Создаем второй аккаунт.
+
+            account_2.AccountType = AccountType.SavingAccount; // Воспользуемся свойством, чтобы установить тип банковского счета.
+
+            // Выводим инфорамацию:
+            Console.WriteLine("===== First Account ====="); 
+            account_1.GetInfo(); 
+            Console.WriteLine(); 
+
+            Console.WriteLine("===== Second Account =====");
+            account_2.GetInfo();
+            Console.WriteLine(); 
+
+            // Транзакция:
+            Console.WriteLine("Выполним перевод в размере 2500 с первого аккаунта на второй: "); 
+            account_1.MoneyTaransaction(account_2, 2500);
+            Console.WriteLine("Результат:");
+
+            // Снова выводим информацию:
+            Console.WriteLine("===== First Account =====");
+            account_1.GetInfo();
+            Console.WriteLine();
+
+            Console.WriteLine("===== Second Account =====");
+            account_2.GetInfo();
+            Console.WriteLine();
+
+            // Вызов метода переворота строки:
+            Console.Write("Введите строку, котору следует перевернуть: ");
+            string str = Console.ReadLine();
+            Console.WriteLine($"Получим: {ReverseString(str)}");
+        }
+        static string ReverseString(string str) // Метод, переворачивающий входную строку.
+        {
+            string reversedSring = String.Empty;
+            for (int i = str.Length-1; i >= 0; i--)
+            {
+                reversedSring += str[i];
+            }
+            return reversedSring;
+        }
+    }
+}
